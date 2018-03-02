b1b23ac1aa67ff9c0f6fe0cd14f149be06211863
CRKarthick <36162358+CRKarthick@users.noreply.github.com>
2018-03-02 19:22:25 +0530

    Update set1-3.py

--- set1-3.py
+++ set1-3.py
@@ -2,7 +2,7 @@ ch = input("Input a character:")
 
 if ch in ('a', 'e', 'i', 'o', 'u'):
     print("It is a vowel.")
-elif(int(ch)>=0 and int(ch)<=9):
+elif ch in ('0','1','2','3','4','5','6','7','8','9'):
         print("It is a number")
 else:
     print("It is a consonant.")
