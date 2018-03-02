753d4f07fe878db7690ed4b60d43ced89b9cd8f7
CRKarthick <36162358+CRKarthick@users.noreply.github.com>
2018-03-02 19:34:34 +0530

    Update set1-4.py

--- set1-4.py
+++ set1-4.py
@@ -2,7 +2,7 @@ ch = input("Enter a character: ")
 if ('a' <= ch <= 'z') or ('A' <= ch <= 'Z'):
     print(ch, "is an Alphabet")
 elif '0'<=ch<= '9':
-    print("It is not an Alphabet")
+    print(ch, "is not an Alphabet")
 
 else:
-    print(ch, "Invalid input")
+    print(`"Invalid input")
