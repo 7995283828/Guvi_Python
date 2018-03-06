36070c806601e17b777463c70f33cc0e487b713b
CRKarthick <36162358+CRKarthick@users.noreply.github.com>
2018-03-06 12:42:52 +0530

    Update 18.py

--- set2/18.py
+++ set2/18.py
@@ -1,1 +1,11 @@
-
+num1 = int(input("Enter starting range: "))
+num2 = int(input("Enter ending range: "))
+for num in range(num1+1,num2):
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+   if num == sum:
+       print(num)
