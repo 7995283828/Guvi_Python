310d17b51a75284c65f3e4f6f119337424e2a835
CRKarthick <36162358+CRKarthick@users.noreply.github.com>
2018-03-25 20:45:36 +0530

    Find the maximum among 10 numbers

--- set6/59.py
+++ set6/59.py
@@ -1,7 +1,7 @@
-a=[]
+lst=[]
 b=10
 for i in range(1,b+1):
     c=int(input(" "))
-    a.append(c)
-a.sort()
-print("Largest Element is:",a[b-1])
+    lst.append(c)
+lst.sort()
+print("Largest Element is:",lst[b-1])
