a5b05989834797797ca6fc09dc2b0556bd767639
CRKarthick <36162358+CRKarthick@users.noreply.github.com>
2018-05-23 12:13:51 +0530

    kabali case study

---
+++ set9/81.py
@@ -0,0 +1,6 @@
+a=int(input("Enter kabali clan's members:"))
+b=int(input("Enter the opponent clan's members:"))
+if a<b:
+  print(b-a)
+else:
+  print("Kabali's ninja number is never greater than his opponent.")
